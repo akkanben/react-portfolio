90f6c05bcfd69df7f9dad040ecd61211ec7a5f43
Ben Mills <fuzzyllama@gmail.com>
2021-10-30 09:59:16 -0700

    Adds detail to skills, projects and about me

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Time Estimates
 
-| # | Feature Name       | Estimated Time | Start   | Finish | Actual |
-| - | ------------------ | -------------- | ------- | ------ | ------ |
-| 1 | Clone and Deploy   | 15 mintues     | 9:30PM  |        |        |
-| 2 | About Me Page      |                |         |        |        |
-| 3 | Projects Section   |                |         |        |        |
-| 4 | Skill & Experience |                |         |        |        |
-| 5 | Personalize        |                |         |        |        |
+| # | Feature Name       | Estimated Time |  Start   | Finish  | Actual |
+| - | ------------------ | -------------- | -------- | ------- | ------ |
+| 1 | Clone and Deploy   | 00:15          | 09:30PM  | 10:05PM | 00:35  |
+| 2 | About Me Page      | 00:30          | 09:25AM  | 09:55AM | 00:30  |
+| 3 | Projects Section   | 00:30          | 08:45AM  | 09:25AM | 00:40  |
+| 4 | Skill & Experience |                |          |         |        |
+| 5 | Personalize        |                |          |         |        |
 
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import React, {Component} from "react";
+import {Icon} from "@iconify/react";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import javascriptIcon from '@iconify/icons-logos/javascript';
+import bashIcon from '@iconify/icons-logos/bash-icon';
 
 class About extends Component {
   render() {
@@ -18,29 +18,29 @@ class About extends Component {
     return (
       <section id="about">
         <div className="col-md-12">
-          <h1 style={{ color: "black" }}>
+          <h1 style={{color: "black"}}>
             <span>{sectionName}</span>
           </h1>
           <div className="row center mx-auto mb-5">
             <div className="col-md-4 mb-5 center">
               <div className="polaroid">
-                <span style={{ cursor: "auto" }}>
+                <span style={{cursor: "auto"}}>
                   <img
                     height="250px"
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    icon={bashIcon}
+                    style={{fontSize: "400%", margin: "9% 5% 0 5%"}}
                   />
                   <Icon
                     icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    style={{fontSize: "400%", margin: "9% 5% 0 5%"}}
                   />
                   <Icon
-                    icon={vueIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    icon={javascriptIcon}
+                    style={{fontSize: "400%", margin: "9% 5% 0 5%"}}
                   />
                 </span>
               </div>
@@ -77,7 +77,7 @@ class About extends Component {
                     }}
                   >
                     <br />
-                    <span className="wave">{hello} :) </span>
+                    <span className="wave">{hello}</span>
                     <br />
                     <br />
                     {about}
